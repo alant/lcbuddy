7b527ea0f0ad5608cb0606fa4d34fa889db54664
alant <alan.htang@gmail.com>
2020-08-30 20:31:16 -0700

    minor change

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,19 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <TodoList/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import TodoList from './components/TodoList';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    TodoList,
   },
 
   data: () => ({
